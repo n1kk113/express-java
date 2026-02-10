313fecd0c17774728e1b52632599c376ec4ddee3
n1kk113 <n1kk1.nikk@yandex.ru>
2026-02-10 23:06:05 +0300

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,5 +14,43 @@ public class Main {
         Book book = new Book("Тестирование на проникновение", "Джоши Пранав");
         book.setAuthor("Чанда Дипаян");
         book.printInfo();
+
+        // Банковский аккаунт //
+        BankAccount bankAccount = new BankAccount("Denis Nikitin", 200);
+        bankAccount.deposit(200);
+        bankAccount.withdraw(100);
+        bankAccount.printBalance();
+
+        // Координаты //
+        Point point = new Point(3,10);
+        point.setX(8);
+        point.print();
+
+        // Cтуденческая группа //
+        StudentGroup studentGroup = new StudentGroup("Developer", 2);
+        studentGroup.setStudentCount(10);
+        studentGroup.printInfo();
+
+        // Окружность //
+        Circle circle = new Circle(3.0);
+        circle.setRadius(8.0);
+        System.out.println("Площадь: " + circle.calculateArea());
+        System.out.println("Длина окужности: " + circle.calculateCircumference());
+
+        // Учитель и предмет //
+        Teacher teacher = new Teacher("Мария Ивановна", "Русский язык");
+        teacher.setSubject("Математика");
+        teacher.printInfo();
+
+        // Продукт //
+        Product product = new Product("PS5",45000.0);
+        product.setPrice(43000.0);
+        product.applyDiscount(2);
+        product.printInfo();
+
+        // Ноутбук //
+        Laptop laptop = new Laptop("MSI", 100000.0);
+        laptop.setPrice(110000);
+        laptop.printInfo();
     }
 }
